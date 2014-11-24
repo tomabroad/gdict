a8f1a736a4ab9b9609ea6e4e98be05922987f9ef
Tomotaro Uejima <polar_bear1205@yahoo.co.jp>
2014-11-24 17:43:48 +0900

    remove unused code

--- js/background.js
+++ js/background.js
@@ -21,15 +21,3 @@ chrome.runtime.onInstalled.addListener(function() {
   var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                          "id": "context" + context});  
 });
-
-// not used yet
-function appendTemporaryDefinition(query) {
-  var encodedQuery = encodeURIComponent(query);
-  var url = "http://services.aonaware.com/DictService/DictService.asmx/Define?word=" + encodedQuery;
-  console.log(url);
-  
-  //$.get(url, function(data, status) {
-    // window.open(url, '_blank');
-    // alert(data);
-  //});
-}
